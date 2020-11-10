ebc3db49cd738e2c7b52e5c0a0663dd9e304a402
Hector Zurga <hjzurga@gmail.com>
2020-11-10 11:02:00 -0400

    fix: Scaffold tests, now evaluating redux state

--- src/components/scaffold/Scaffold.test.jsx
+++ src/components/scaffold/Scaffold.test.jsx
@@ -1,42 +1,47 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
 import Scaffold from './Scaffold';
+import { Provider } from 'react-redux';
+import store from '../../redux/store';
 import { render } from '../../utils/test-utils';
 import { trans } from '../../trans/trans';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-test('should renders Scaffold component', () => {
-    const scaffold = renderer.create(
-        <Router>
-            <Scaffold>
-                <p>Test</p>
-            </Scaffold>
-        </Router>
-    );
+const ScaffoldWithProvider = () => (
+	<Provider store={store()}>
+		<Scaffold>
+			<p>Test</p>
+		</Scaffold>
+	</Provider>
+);
 
-    expect(scaffold.toJSON()).toMatchSnapshot();
+test('should renders Scaffold component', () => {
+	const scaffold = renderer.create(
+		<Router>
+			<ScaffoldWithProvider />
+		</Router>
+	);
+
+	expect(scaffold.toJSON()).toMatchSnapshot();
 });
 
 test('should find title in appbar', async () => {
-    const { getByText } = render(
-        <Router>
-            <Scaffold>
-                <p>Test</p>
-            </Scaffold>
-        </Router>
-    );
+	const { getByText } = render(
+		<Router>
+			<ScaffoldWithProvider />
+		</Router>
+	);
 
-    expect(getByText(trans('Components.scaffold.title'))).toBeInTheDocument();
+	expect(getByText(trans('Components.scaffold.title'))).toBeInTheDocument();
 });
 
 test('should find child', async () => {
-    const { getByText } = render(
-        <Router>
-            <Scaffold>
-                <p>Test</p>
-            </Scaffold>
-        </Router>
-    );
+	const { getByText } = render(
+		<Router>
+			<ScaffoldWithProvider />
+		</Router>
+	);
 
-    expect(getByText('Test')).toBeInTheDocument();
+	expect(getByText('Test')).toBeInTheDocument();
 });
+
