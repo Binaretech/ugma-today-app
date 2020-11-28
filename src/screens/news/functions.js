bc68b56453e43b545eb66af1f309df21c6a4dc4e
Angel Afonso <angelafonso60@gmail.com>
2020-11-28 17:33:27 -0400

    feat: added test cases

--- src/screens/news/functions.js
+++ src/screens/news/functions.js
@@ -1,25 +1,31 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useXhr } from "../../utils/xhr/hook";
 import requests from "../../utils/xhr/requests";
 
 export function useFetchNews() {
   const [send] = useXhr(requests.news.index);
-  const fetchPosts = useCallback(send, []);
 
   const [loading, setLoading] = useState(false);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
     setLoading(true);
-    fetchPosts().then((response) => {
-      setLoading(false);
-      setPosts(
-        response?.ids?.map((id) => {
-          return response?.data?.[id];
-        }) ?? []
-      );
-    });
-  }, [fetchPosts]);
+    send()
+      .then((response) => {
+        console.log(response);
+        setLoading(false);
+        setPosts(
+          response?.ids?.map((id) => {
+            return response?.data?.[id];
+          }) ?? []
+        );
+      })
+      .catch((e) => {
+        console.log(e);
+        setLoading(false);
+      });
+    //eslint-disable-next-line
+  }, []);
 
   return [loading, posts];
 }
