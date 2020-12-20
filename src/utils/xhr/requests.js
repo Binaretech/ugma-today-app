9b01565438e7aaf8764e659900bd620f09311535
Angel Afonso <angelafonso60@gmail.com>
2020-12-20 14:51:07 -0400

    removed duplicated keys

--- src/utils/xhr/requests.js
+++ src/utils/xhr/requests.js
@@ -28,16 +28,6 @@ const endpoints = {
       url: '/post/:id/comment',
       showErrorSnackbar: true,
     },
-    like: {
-      method: 'POST',
-      url: '/post/like/:id',
-      showErrorSnackbar: true,
-    },
-    unlike: {
-      method: 'POST',
-      url: '/post/unlike/:id',
-      showErrorSnackbar: true,
-    },
   },
   news: {
     index: {
