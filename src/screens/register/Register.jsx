a061097a8f41756ab7eb8977f55b793bc1f3104c
Hector Zurga <35118597+HectorZR@users.noreply.github.com>
2021-01-20 08:43:09 -0400

    Improved validation rules in password (#21)
    * fix: styles
    
    - In Scaffold
    - In Register
    - In Login
    
    * initial instalations
    
    * fix: dev dependencies
    
    * improved rules in password input
    
    * fixing icons and manifest.json
    
    Co-authored-by: Angel Afonso <angelafonso60@gmail.com>

--- src/screens/register/Register.jsx
+++ src/screens/register/Register.jsx
@@ -15,114 +15,127 @@ import { trans } from '../../trans/trans';
 import styles from './Register.module.css';
 
 function Register() {
-	const [loading, setLoading] = useState(false);
-	const inputValues = {};
-	const manager = useDataManager(inputValues);
-	const dispatch = useDispatch();
-	const history = useHistory();
-	const [send] = useXhr({
-		url: apiEndpoints.register,
-		method: 'POST',
-		showErrorSnackbar: true,
-	});
+  const [loading, setLoading] = useState(false);
+  const inputValues = {};
+  const manager = useDataManager(inputValues);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [send] = useXhr({
+    url: apiEndpoints.register,
+    method: 'POST',
+    showErrorSnackbar: true,
+  });
 
-	function onSubmit() {
-		if (manager.hasErrors()) return;
+  function onSubmit() {
+    if (manager.hasErrors()) return;
 
-		setLoading(true);
-		send({
-			body: { ...manager.getData() },
-		})
-			.then((res) => {
-				setLoading(false);
-				dispatch(setLogin(res.data));
-				history.push(paths.home);
-			})
-			.catch((err) => {
-				setLoading(false);
-				dispatch(snackbarMessage(err.message));
-				console.error(err);
-			});
-	}
+    setLoading(true);
+    send({
+      body: { ...manager.getData() },
+    })
+      .then((res) => {
+        setLoading(false);
+        dispatch(setLogin(res.data));
+        history.push(paths.home);
+      })
+      .catch((err) => {
+        setLoading(false);
+        dispatch(snackbarMessage(err.message));
+        console.error(err);
+      });
+  }
 
-	return (
-		<div className={styles.container}>
-			{(loading && <Loader fullscreen />) || (
-				<form className={styles.form}>
-					<Input
-						label={trans('words.name')}
-						name="name"
-						variant="outlined"
-						value={inputValues.name}
-						rules={['required', 'min:3']}
-						setValue={manager.setValue}
-						setError={manager.setError}
-					/>
-					<Input
-						label={trans('words.lastname')}
-						name="lastname"
-						variant="outlined"
-						value={inputValues.lastname}
-						rules={['required', 'min:3']}
-						setValue={manager.setValue}
-						setError={manager.setError}
-					/>
-					<Input
-						label={trans('words.user')}
-						name="username"
-						variant="outlined"
-						value={inputValues.username}
-						rules={['required']}
-						setValue={manager.setValue}
-						setError={manager.setError}
-					/>
-					<Input
-						label={trans('words.email')}
-						name="email"
-						variant="outlined"
-						value={inputValues.email}
-						rules={['required', 'email']}
-						setValue={manager.setValue}
-						setError={manager.setError}
-					/>
-					<Input
-						label={trans('words.password')}
-						name="password"
-						type="password"
-						variant="outlined"
-						value={inputValues.password}
-						rules={['required']}
-						setValue={manager.setValue}
-						setError={manager.setError}
-					/>
-					<Input
-						label={trans('words.confirmPassword')}
-						name="password_confirmation"
-						type="password"
-						variant="outlined"
-						value={inputValues.password}
-						rules={[
-							'required',
-							{
-								message: trans('Screens.Register.confirmPassword'),
-								validation: (value) =>
-									confirmedPassword(value, manager.getData()?.password),
-							},
-						]}
-						setValue={manager.setValue}
-						setError={manager.setError}
-					/>
-					<Button variant="contained" onClick={onSubmit}>
-						{trans('Screens.Register.registerButton')}
-					</Button>
-				</form>
-			)}
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      {(loading && <Loader fullscreen />) || (
+        <form className={styles.form}>
+          <Input
+            label={trans('words.name')}
+            name="name"
+            variant="outlined"
+            value={inputValues.name}
+            rules={['required', 'min:3']}
+            setValue={manager.setValue}
+            setError={manager.setError}
+          />
+          <Input
+            label={trans('words.lastname')}
+            name="lastname"
+            variant="outlined"
+            value={inputValues.lastname}
+            rules={['required', 'min:3']}
+            setValue={manager.setValue}
+            setError={manager.setError}
+          />
+          <Input
+            label={trans('words.user')}
+            name="username"
+            variant="outlined"
+            value={inputValues.username}
+            rules={['required']}
+            setValue={manager.setValue}
+            setError={manager.setError}
+          />
+          <Input
+            label={trans('words.email')}
+            name="email"
+            variant="outlined"
+            value={inputValues.email}
+            rules={['required', 'email']}
+            setValue={manager.setValue}
+            setError={manager.setError}
+          />
+          <Input
+            label={trans('words.password')}
+            name="password"
+            type="password"
+            variant="outlined"
+            value={inputValues.password}
+            rules={[
+              'required',
+              'min:6',
+              'max:45',
+              {
+                message: trans('validation.notRegex'),
+                validation: (value) => validateSecuenceInPassword(value),
+              },
+            ]}
+            setValue={manager.setValue}
+            setError={manager.setError}
+          />
+          <Input
+            label={trans('words.confirmPassword')}
+            name="password_confirmation"
+            type="password"
+            variant="outlined"
+            value={inputValues.password}
+            rules={[
+              'required',
+              {
+                message: trans('Screens.Register.confirmPassword'),
+                validation: (value) =>
+                  confirmedPassword(value, manager.getData()?.password),
+              },
+            ]}
+            setValue={manager.setValue}
+            setError={manager.setError}
+          />
+          <Button variant="contained" onClick={onSubmit}>
+            {trans('Screens.Register.registerButton')}
+          </Button>
+        </form>
+      )}
+    </div>
+  );
 }
 
 export default Register;
 
 function confirmedPassword(value, valueToConfirm) {
-	return value === valueToConfirm;
+  return value === valueToConfirm;
+}
+
+function validateSecuenceInPassword(value) {
+  if (value === '123456' || value === 'abcdef') return false;
+  return true;
 }
