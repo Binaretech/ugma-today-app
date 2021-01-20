4cbd1c24b36ce5c63d671062968b826fb1fb60cd
Hector Zurga <hjzurga@gmail.com>
2021-01-20 00:14:32 -0400

    improved rules in password input

--- src/trans/es/validation.js
+++ src/trans/es/validation.js
@@ -1,8 +1,9 @@
 export default {
-	required: 'Es requerido.',
-	string: 'Debe ser una cadena de texto',
-	number: 'Debe ser numerico',
-	min: 'Debe ser de al menos :0 caracteres',
-	max: 'Debe ser de maximo :0 caracteres',
-	email: 'No es un correo electrónico válido.',
+  required: 'Es requerido.',
+  string: 'Debe ser una cadena de texto',
+  number: 'Debe ser numerico',
+  min: 'Debe ser de al menos :0 caracteres',
+  max: 'Debe ser de maximo :0 caracteres',
+  email: 'No es un correo electrónico válido.',
+  notRegex: 'Patrón de caracteres inválido',
 };
