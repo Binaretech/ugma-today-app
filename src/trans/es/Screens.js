2e0b2659420f66f452d8a96f0e77bad8e166b7ee
Hector Zurga <hjzurga@gmail.com>
2021-02-01 13:21:27 -0400

    Fix: added password instructions in register view

--- src/trans/es/Screens.js
+++ src/trans/es/Screens.js
@@ -1,12 +1,16 @@
 export default {
-	Login: {
-		loginButton: 'Iniciar sesión',
-	},
-	ListCosts: {
-		rowsPerPage: 'Registros por página',
-	},
-	Register: {
-		registerButton: 'Registrarse',
-		confirmPassword: 'Las contraseñas no coinciden',
-	},
+  Login: {
+    loginButton: 'Iniciar sesión',
+  },
+  ListCosts: {
+    rowsPerPage: 'Registros por página',
+  },
+  Register: {
+    registerButton: 'Registrarse',
+    confirmPassword: 'Las contraseñas no coinciden',
+    minimunCharacters: 'La contraseña debe tener mínimo 6 caracteres.',
+    maximunCharacters: 'La contraseña debe tener máximo 45 caracteres.',
+    incorrectPatron: 'No se adminiten patrones como: abcdef o 123456.',
+    include: 'Incluya mayúsculas, caracteres especiales y números.',
+  },
 };
